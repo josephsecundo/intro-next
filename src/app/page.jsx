9016ba825ac9d70f5a70f08b7a380635f60f6d9f
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-07-30 22:45:17 -0300

    att styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1>Aula Next</h1>
+    <div className='container'>
+    <h1 className={styles.titulo}>Aula Next</h1>
+    </div>
     );
 }
