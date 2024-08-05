a45adb7143ae7a45ab90a934196c39beb458979b
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-08-05 20:24:40 -0300

    Criando Rotas

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Sobre() {
   return (
     <div className='container'>
     <div className={styles.containerTitulo}>
-      <h1 className={styles.txtTitulo}>Aula Next</h1>
+      <h1 className={styles.txtTitulo}>Sobre</h1>
       </div>
     </div>
     );
--- src/app/usuarios/login/page.jsx
+++ src/app/usuarios/login/page.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Login() {
   return (
     <div className='container'>
     <div className={styles.containerTitulo}>
-      <h1 className={styles.txtTitulo}>Aula Next</h1>
+      <h1 className={styles.txtTitulo}>Login</h1>
       </div>
     </div>
     );
